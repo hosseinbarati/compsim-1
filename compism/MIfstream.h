cdbe7d74421ba9e982c5154b0bc299710465d747
Ali Reza Khaz'ali <user.name>
2015-03-17 00:18:21 +0330

    new class added

---
+++ compism/MIfstream.h
@@ -0,0 +1,51 @@
+#include <fstream>
+#include <iostream>
+#include <string.h>
+
+class MIfstream: public std::ifstream {
+public:
+	int FileSearch(char *);
+	int ReadWord(char *);
+};
+
+
+int MIfstream::FileSearch(char *rSeek) {
+	register int i;
+	char str[MAX_STRING_LENGTH];
+
+	clear();                 // clear fail and eof bits
+	seekg(0, std::ios::beg); // back to the start!
+	*str='\0';
+	do {
+		i=ReadWord(str);
+		if (!strcmp(str, rSeek)) return -1;
+	} while (i);
+
+	return 0;		//Nothing found
+}
+
+int MIfstream::ReadWord(char *rWord) {
+	char ch;
+	register int i=0;
+
+	*rWord='\0';
+	do {
+		std::ifstream::get(ch);
+		if (std::ifstream::eof()) {
+			return 0;		//Nothing has been read
+		}
+	} while ((ch<33) || (ch>126));
+
+	while ((ch>32) && (ch<127)) {
+		*(rWord+i)=ch;
+		i++;
+		std::ifstream::get(ch);
+		if (std::ifstream::eof()) {
+			*(rWord+i)='\0';
+			return 1;		//Read, but end of file also encountered
+		}		
+	}
+
+	*(rWord+i)='\0';
+	return -1;		//Correct execution
+}
